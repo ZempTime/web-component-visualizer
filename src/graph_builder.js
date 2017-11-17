8a1ca5bd94f6b283a8fcad02d1ca8a175becf30f
Chris Zempel <zempel@efeqdev.com>
2017-11-17 15:47:42 -0600

    Adds more things. Still WIP, trying to figure out whats happening

---
+++ src/graph_builder.js
@@ -0,0 +1,14 @@
+var vertices = [];
+var edges = [];
+
+function main() {
+  debugger;
+  console.log("Traversing ", items.length, "items");
+  document.getElementById("counter").innerHtml = items.length.toString();
+  for (var i = items.length; i--;) {
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  main();
+});
